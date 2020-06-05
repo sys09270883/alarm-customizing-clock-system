cd8d2f05211ee963b8811067fc843f7aa0fa7dc4
kkalkkkal <kmh0207@konkuk.ac.kr>
2020-06-05 17:48:47 +0900

    SystemTest : R7

--- src/test/java/SystemTest.java
+++ src/test/java/SystemTest.java
@@ -10,6 +10,7 @@ public class SystemTest  {
 
         system.selectFunction();
 
+        // 화면이 잘 넘어가는지 확인 : 1,2,5,6  설정순
         java.lang.System.out.println(system.getSelectedFid());
         system.nextFunction();
         java.lang.System.out.println(system.getSelectedFid());
@@ -36,35 +37,52 @@ public class SystemTest  {
         Alarm alarm = new Alarm(system);
         AlarmCustom alarmCustom = new AlarmCustom(system);
 
-
+        // 타임 키핑
         java.lang.System.out.println(system.getSelectedFid());
         timeKeeping.requestTimeSettingMode();
         system.startCheckTimeOut();
         // (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
-        system.getMode();
+        java.lang.System.out.println(system.getMode());
 
-
+        // 스톱워치
         java.lang.System.out.println(system.getSelectedFid());
         stopwatch.requestRecordCheckMode();
         system.startCheckTimeOut();
+// (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
+        java.lang.System.out.println(system.getMode());
+        java.lang.System.out.println(system.getSelectedFid());
 
+        // 타이머
         java.lang.System.out.println(system.getSelectedFid());
         timer.requestTimerSettingMode();
         system.startCheckTimeOut();
+// (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
+        java.lang.System.out.println(system.getMode());
+        java.lang.System.out.println(system.getSelectedFid());
 
+        // 디데이
         java.lang.System.out.println(system.getSelectedFid());
         d_day.requestDdaySettingMode();
         system.startCheckTimeOut();
+// (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
+        java.lang.System.out.println(system.getMode());
 
+        // 알람
 
         java.lang.System.out.println(system.getSelectedFid());
         alarm.requestAlarmSettingMode();
         system.startCheckTimeOut();
+// (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
+        java.lang.System.out.println(system.getMode());
+        java.lang.System.out.println(system.getSelectedFid());
 
+        // 알람 커스텀
         java.lang.System.out.println(system.getSelectedFid());
         alarmCustom.requestIntervalSettingMode();
         system.startCheckTimeOut();
-
+// (임시로 10분을 10초로 바꾸고) 10초 정지 후 어떻게 되는 지 확인
+        system.getMode();
+        java.lang.System.out.println(system.getSelectedFid());
     }
 
     @Test
@@ -79,40 +97,40 @@ public class SystemTest  {
         //타임 키핑
         timeKeeping.requestTimeSettingMode();
         system.cancel(timeKeeping);
-        java.lang.System.out.println(timeKeeping.getMode());
+        java.lang.System.out.println(timeKeeping.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         // 스톱워치
         stopwatch.requestRecordCheckMode();
         system.cancel(stopwatch);
-        java.lang.System.out.println(stopwatch.getMode());
+        java.lang.System.out.println(stopwatch.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         //타이머
         timer.requestTimerSettingMode();
         system.cancel(timer);
-        java.lang.System.out.println(timer.getMode());
+        java.lang.System.out.println(timer.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         //디데이
         d_day.requestDdaySettingMode();
         system.cancel(d_day);
-        java.lang.System.out.println(d_day.getMode());
+        java.lang.System.out.println(d_day.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         // 알람
         alarm.requestAlarmSettingMode();
         system.cancel(alarm);
-        java.lang.System.out.println(alarm.getMode());
+        java.lang.System.out.println(alarm.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         // 알람 커스텀
         alarmCustom.requestIntervalSettingMode();
         system.cancel(alarmCustom);
-        java.lang.System.out.println(alarmCustom.getMode());
+        java.lang.System.out.println(alarmCustom.getMode()); // 기본 단계로 돌아갔는지 확인.
 
         alarmCustom.requestAlarmVolumeMode();
         system.cancel(alarmCustom);
-        java.lang.System.out.println(alarmCustom.getMode());
+        java.lang.System.out.println(alarmCustom.getMode());// 기본 단계로 돌아갔는지 확인.
 
         alarmCustom.requestAlarmSelectMode();
         system.cancel(alarmCustom);
-        java.lang.System.out.println(alarmCustom.getMode());
+        java.lang.System.out.println(alarmCustom.getMode()); // 기본 단계로 돌아갔는지 확인.
 
 
     }
